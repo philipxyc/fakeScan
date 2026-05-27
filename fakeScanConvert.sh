c54bb1cd9e3dbb36eba3aa816d4658fc0fd85d05
Philip Xia <philipxyc@live.com>
2026-05-27 09:32:30 -0700

    Update fakeScanConvert.sh

--- fakeScanConvert.sh
+++ fakeScanConvert.sh
@@ -1,7 +1,1 @@
-convert -density 300 input.pdf \
-        -colorspace RGB -background white -alpha remove \
-        -attenuate 2 +noise Gaussian \
-        -blur 0x0.8 \
-        -rotate 0.4 \
-        -compress JPEG -quality 85 \
-        output_scanned.pdf
+f=input.pdf; convert -density 300 "$f" -colorspace RGB -background white -alpha remove -attenuate 2 +noise Gaussian -blur 0x0.8 -rotate 0.4 -compress JPEG -quality 85 "${f%.pdf}_scanned.pdf"
